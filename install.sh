b39a5971103b41c52aa45ecc3e5087a409a348db
Robert Dorna <rdorna8@gmail.com>
2023-05-18 22:00:03 +0200

    feat: add Xresources (URxvt) and i3 config to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 ln -is $PWD/config/bashrc ~/.bashrc
 ln -is $PWD/config/gitconfig ~/.gitconfig
+ln -is $PWD/config/Xresources ~/.Xresources
+ln -is $PWD/config/i3 ~/.config/i3
 ln -iTs $PWD/config/nvim ~/.config/nvim
 ln -iTs $PWD/config/nixpkgs ~/.config/nixpkgs
 ln -iTs $PWD/config/background-image ~/.background-image
