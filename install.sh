67d04f4a59abbc38dc3dcade579fd86be36a8a1e
Robert Dorna <rdorna8@gmail.com>
2023-05-19 10:03:40 +0200

    feat: remove background-image from config/

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ ln -is $PWD/config/i3 ~/.config/i3
 ln -iTs $PWD/config/nvim ~/.config/nvim
 ln -iTs $PWD/config/nixpkgs ~/.config/nixpkgs
 ln -iTs $PWD/config/background-image ~/.background-image
+
+echo "go to config/ and create a symlink to ../wallpapers/ that you want named 'background-image'"
