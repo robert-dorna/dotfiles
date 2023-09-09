089b53379f3ce228c27fc940a7b85aa2869c5fa4
Robert Dorna <rdorna8@gmail.com>
2023-09-09 11:27:25 +0200

    feat: nix open ports 8080 8081

--- nixos/host-shared.nix
+++ nixos/host-shared.nix
@@ -9,7 +9,7 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 80 443 19000 19001 19002 ];
+      allowedTCPPorts = [ 80 443 8080 8081 19000 19001 19002 ];
       allowedUDPPortRanges = [
         { from = 4000; to = 4007; }
         { from = 8000; to = 8010; }
